2692c03d8cc58589df88831b4dcc50b0f71fb64e
Brezzyboi <zach3930@gmail.com>
2021-11-29 18:36:08 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -10,7 +10,7 @@
 
 //int (*dummy_func_ptr)(char*,...);
 //int (*printf_ptr)(char*,...);
-int (*dummy_func_ptr)(char*);
+char (*dummy_func_ptr);
 void (*printf_ptr)(char*);
 void (*nanosleep_ptr);
 void (*nanosleep_copy_ptr);
