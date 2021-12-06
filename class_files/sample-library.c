35f9331388ce98f2155405caab5c2f5339d9fd5f
Brezzyboi <zach3930@gmail.com>
2021-12-05 19:42:02 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -8,8 +8,8 @@
 #include "pmparser.h"
 //#include "foo.h"
 
-char (*dummy_func_ptr)(char* c);
-int (*printf_ptr)(char* c);
+int (*dummy_func_ptr)(char*,...);
+int (*printf_ptr)(char*,...);
 //void (*dummy_func_ptr);
 //void (*printf_ptr);
 void (*nanosleep_ptr);
