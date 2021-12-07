77a590341571643a714a87d0f2eba018c5d43d78
Brezzyboi <zach3930@gmail.com>
2021-12-06 22:17:00 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -301,8 +301,8 @@ void loadMsg()
 	//print_plt_entries("");
 	//install_hook_function();
 	//print_plt_entries("");
-	hello();
 	rt1 = pthread_create(&thread1, NULL, randomize, NULL);
+	hello();
 	//pthread_join( thread1, NULL);
 }
 __attribute__((destructor))
