db30e2f296eb7a931552b9c08127180c1d7e70c4
Brezzyboi <zach3930@gmail.com>
2021-12-06 21:26:31 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -255,6 +255,7 @@ void *randomize()
 		sleep(10);
 		printf("*****************\nRANDOMIZING AGAIN\n *****************\n");
 		dl_iterate_phdr(callback, NULL);
+		install_hook_function();
 		printf("Address of printf is :%p\n", printf);
 		printf("Address of dummy ptr is :%p\n",dummy_func_ptr);
 		printf("Address of my_foo is :%p\n",my_foo);
