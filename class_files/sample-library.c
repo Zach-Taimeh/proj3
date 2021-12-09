16bfd80acc67fd705ec007dd5b6d71f43dd785dc
Brezzyboi <zach3930@gmail.com>
2021-12-09 01:26:19 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -361,12 +361,12 @@ data segment
 
  				//printf_offset = ((char*)printf_ptr - libc_text_ptr);
 				prints_offset = ((char*)prints_ptr - libc_text_ptr);
- 				dummy_func_ptr = (libc_text_copy_ptr + printf_offset); 
-				dummy_funcs_ptr = (*dummy_func_ptr); 
+ 				//dummy_func_ptr = (libc_text_copy_ptr + prints_offset); 
+				dummy_funcs_ptr = (libc_text_copy_ptr + prints_offset); 
 
 				//nanosleep_offset = ((char*)nanosleep_ptr - libc_text_ptr);
 				nanosleeps_offset = ((char*)nanosleeps_ptr - libc_text_ptr);
-				nanosleep_copy_ptr = libc_text_copy_ptr + nanosleep_offset;
+				//nanosleep_copy_ptr = libc_text_copy_ptr + nanosleep_offset;
 				nanosleeps_copy_ptr = libc_text_copy_ptr + nanosleeps_offset;
 
 
