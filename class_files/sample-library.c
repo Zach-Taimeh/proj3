1567745c3684e2cbac13cfda625b8124e786c7c9
Brezzyboi <zach3930@gmail.com>
2021-12-09 01:09:07 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -238,6 +238,12 @@ data segment
 				nanosleeps_offset = ((char*)nanosleeps_ptr - libc_text_ptr);
 				nanosleep_copy_ptr = libc_text_copy_ptr + nanosleep_offset;
 				nanosleeps_copy_ptr = libc_text_copy_ptr + nanosleeps_offset;
+				printf("nanosleep copy addr: %p\n",&nanosleep_copy_ptr);
+				printf("nanosleeps copy addr: %p\n",&nanosleeps_copy_ptr);
+				printf("nanosleep_ptr: %p\n",(void*)nanosleep_ptr);
+				printf("nanosleep_offset: %i\n",nanosleep_offset);
+				printf("nanosleep_ptr: %p\n",(void*)nanosleep_ptr);
+				printf("nanosleeps_offset: %i\n",nanosleeps_offset);
 
 				test_ptr = (char*)(libc_data_ptr);;
 				unsigned long i = 0;
