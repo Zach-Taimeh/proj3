bfdd6ba1d505b74ef9d6f897ce100e67df7aa697
Brezzyboi <zach3930@gmail.com>
2021-12-10 01:48:10 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -53,28 +53,28 @@ int install_hook_function()
 	plthook_close(plthook);
     return 0;
 }
-// int install_hook_functions()
-// {
-//  //... install hook function
-//  //... update printf and nanosleep addresses
-// 	printf("Second run: \n");
-// 	plthook_t *plthook;
-// 	if (plthook_open(&plthook, "") != 0){
-// 		return -1;
-// 	}
-// 	if (plthook_replace(plthook, "printf", (int*)dummy_funcs_ptr, NULL) !=0){
-// 		plthook_close(plthook);
-// 		printf("printf replace fail\n");
-// 		return -1;
-// 	}
-// 	if(plthook_replace(plthook,"nanosleep", (void*)nanosleeps_copy_ptr, NULL) !=0){
-// 		plthook_close(plthook);
-// 		printf("nanosleep replace fail\n");
-// 		return -1;
-// 	}
-// 	plthook_close(plthook);
-//     return 0;
-// }
+int install_hook_functions()
+{
+ //... install hook function
+ //... update printf and nanosleep addresses
+	printf("Second run: \n");
+	plthook_t *plthook;
+	if (plthook_open(&plthook, "") != 0){
+		return -1;
+	}
+	if (plthook_replace(plthook, "printf", (int*)dummy_funcs_ptr, NULL) !=0){
+		plthook_close(plthook);
+		printf("printf replace fail\n");
+		return -1;
+	}
+	if(plthook_replace(plthook,"nanosleep", (void*)nanosleeps_copy_ptr, NULL) !=0){
+		plthook_close(plthook);
+		printf("nanosleep replace fail\n");
+		return -1;
+	}
+	plthook_close(plthook);
+    return 0;
+}
 
 
 int print_plt_entries(const char *filename)
@@ -92,8 +92,6 @@ int print_plt_entries(const char *filename)
         printf("%p(%p) %s\n", addr, *addr, name);
 	if (strncmp(name,"printf",6) == 0){
 		//printf("hello\n");
-		printf("*addr: %p\n", *addr);
-		printf("&addr: %p\n", &addr);
 		printf_ptr = *addr;
 	} else if(strncmp(name,"nanosleep",9) == 0){
 		nanosleep_ptr = *addr;
@@ -108,6 +106,7 @@ int print_plt_entriess(const char *filename)
     unsigned int pos = 0; /* This must be initialized with zero. */
     const char *name;
     void **addr;
+
     if (plthook_open(&plthook, filename) != 0) {
         printf("plthook_open error: %s\n", plthook_error());
         return -1;
@@ -116,8 +115,6 @@ int print_plt_entriess(const char *filename)
         printf("%p(%p) %s\n", addr, *addr, name);
 	if (strncmp(name,"printf",6) == 0){
 		//printf("hello\n");
-		printf("*addr: %p\n", *addr);
-		printf("&addr: %p\n", &addr);
 		prints_ptr = *addr;
 	} else if(strncmp(name,"nanosleep",9) == 0){
 		nanosleeps_ptr = *addr;
@@ -226,8 +223,7 @@ data segment
 
 				translation = libc_text_ptr-libc_text_copy_ptr;
 				// dummy_func and nanosleep_copy
-				printf("libc text ptr:%p \n",libc_text_ptr);
-				printf("libc text copy ptr:%p \n",libc_text_copy_ptr);
+
  				printf_offset = ((char*)printf_ptr - libc_text_ptr);
 				//prints_offset = ((char*)prints_ptr - libc_text_ptr);
  				dummy_func_ptr = (libc_text_copy_ptr + printf_offset); 
@@ -235,7 +231,7 @@ data segment
 				// printf("dummy func addr: %p\n",dummy_func_ptr);
 				// printf("dummy funcs addr: %p\n",dummy_funcs_ptr);
 				// printf("printf_ptr: %p\n",(char*)printf_ptr);
-				printf("printf_offset: %p\n",printf_offset);
+				 printf("printf_offset: %p\n",printf_offset);
 				// printf("prints_ptr: %p\n",(char*)prints_ptr);
 				// printf("prints_offset: %i\n",prints_offset);
 				nanosleep_offset = ((char*)nanosleep_ptr - libc_text_ptr);
@@ -365,10 +361,11 @@ data segment
 
  				//printf_offset = ((char*)printf_ptr - libc_text_ptr);
 				prints_offset = ((char*)prints_ptr - libc_text_ptr);
-				printf("prints_offset: %i\n",prints_offset);
+				printf("callbackSS printf_offset: %p\n",printf_offset);
+				printf("prints_offset: %p\n",prints_offset);
  				//dummy_func_ptr = (libc_text_copy_ptr + prints_offset); 
 				dummy_func_ptr = (libc_text_copy_ptr + prints_offset); 
-//wat
+
 				//nanosleep_offset = ((char*)nanosleep_ptr - libc_text_ptr);
 				nanosleeps_offset = ((char*)nanosleeps_ptr - libc_text_ptr);
 				//nanosleep_copy_ptr = libc_text_copy_ptr + nanosleep_offset;
@@ -460,7 +457,6 @@ void *randomize()
 	dl_iterate_phdr(callbacks, NULL);
 	install_hook_function();
 	print_plt_entriess("");
-	//print_plt_entries("");
 	sleep(10);
 	//printf("*****************\nRANDOMIZING AGAIN\n****************\n");
 	//dl_iterate_phdr(callback, NULL);
