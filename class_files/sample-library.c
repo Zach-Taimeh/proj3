c647f644ce2fe0c9755ddc78b7323c0b1c6d3458
Brezzyboi <zach3930@gmail.com>
2021-12-10 03:21:51 -0500

    Update sample-library.c

--- class_files/sample-library.c
+++ class_files/sample-library.c
@@ -204,7 +204,7 @@ data segment
 				if(iter==1){
 					printf_offset = ((char*)prints_ptr - libc_text_ptr);
 					dummy_func_ptr = (libc_text_copy_ptr + prints_offset); 
-					nanosleep_offset = ((char*)nanosleeps_ptr - libc_text_ptr);
+					nanosleep_offset = ((int*)nanosleeps_ptr - libc_text_ptr);
 					nanosleep_copy_ptr = libc_text_copy_ptr + nanosleep_offset;
 					printf("Prints_ptr: %p\n",prints_ptr);
 					printf("prints_offset: %p\n",printf_offset);
